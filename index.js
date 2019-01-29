002e83c25139e46c795a1e7f2bd77c1a94d7f2e1
Jeff Baucom <jeffbaucom@gmail.com>
2019-01-29 14:10:11 -0500

    initialize client

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
-const port = 8000;
+const port = process.env.PORT || 8080;
 let app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
-app.listen(port, function() {
+app.get('/test', function (req, res) {
+  return res.send('test!');
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+});
+app.listen(port, function () {
   console.log(`app listening on port ${port}`);
 });
